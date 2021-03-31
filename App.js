63bc9ccc305bf8a5e16daa87a6fbeacde41e94b6
Sumit Bhardwaj <sumitbhardwaj@Sumits-MacBook-Air.local>
2021-03-31 15:36:11 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Dimensions, View } from 'react-native';
+
+import Pdf from 'react-native-pdf';
 
 export default function App() {
+  const source = {uri:'https://zinedumain.s3.amazonaws.com/notes_file/Linux_exam_codes_agtCMdm.pdf',cache:true};
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Pdf
+            source={source}
+            onLoadComplete={(numberOfPages,filePath)=>{
+                console.log(`number of pages: ${numberOfPages}`);
+            }}
+            onPageChanged={(page,numberOfPages)=>{
+                console.log(`current page: ${page}`);
+            }}
+            onError={(error)=>{
+                console.log(error);
+            }}
+            onPressLink={(uri)=>{
+                console.log(`Link presse: ${uri}`)
+            }}
+            style={styles.pdf}/>
     </View>
-  );
+)
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      flex: 1,
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      marginTop: 25,
   },
+  pdf: {
+      flex:1,
+      width:Dimensions.get('window').width,
+      height:Dimensions.get('window').height,
+  }
 });
